966609ce1104c9a553b3f94d668c77a5706352db
rxw <413038044@qq.com>
2018-09-29 11:10:32 +0800

    1

--- src/main/java/com/giveup/App.java
+++ src/main/java/com/giveup/App.java
@@ -1,11 +1,13 @@
 package com.giveup;
 
+import java.sql.SQLException;
+
 /**
  * Hello world!
  *
  */
 public class App {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
+	public static void main(String[] args) throws SQLException {
+		System.out.println(OtherUtils.buildUrlPath("/sdf/asdf", ""));
 	}
 }
