89ebac15a250046894ca5c4664b8d27c4c8959d0
rxw <413038044@qq.com>
2018-12-10 18:33:56 +0800

    1

--- src/main/java/com/giveup/SplitUtils.java
+++ src/main/java/com/giveup/SplitUtils.java
@@ -1,25 +1,38 @@
 package com.giveup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.log4j.Logger;
 
 public class SplitUtils {
 	public static Logger logger = Logger.getLogger(SplitUtils.class);
 
-	public static String[] toArray(String split, String regex) {
+	public static String[] toArray(String split, String regex, boolean clearEmpty) {
 		if (split == null || split.trim().isEmpty())
 			return new String[] {};
 		else {
 			String[] splitArr = split.split(regex);
+			List<String> ss = new ArrayList();
+			if (clearEmpty) {
+				for (String splitArrEle : splitArr) {
+					if (splitArrEle != null && !splitArrEle.trim().isEmpty())
+						ss.add(splitArrEle);
+				}
+				splitArr = ss.toArray(new String[] {});
+			}
 			return splitArr;
 		}
 	}
 
-	public static String toSplit(String[] splitArr, String regex) {
+	public static String toSplit(String[] splitArr, String regex, boolean clearEmpty) {
 		if (splitArr == null || splitArr.length == 0)
 			return "";
 		StringBuilder s = new StringBuilder();
 		for (int i = 0; i < splitArr.length; i++) {
-			s = s.append(regex).append(splitArr[i] == null ? "" : splitArr[i]);
+			if (clearEmpty && (splitArr[i] == null || splitArr[i].trim().isEmpty()))
+				continue;
+			s = s.append(regex).append(splitArr[i]);
 		}
 		String ss = s.toString();
 		if (ss.length() > 0)
