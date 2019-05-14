a632e9daf1597c894913290ef89928d8adeb412f
renxinwei <413038044@qq.com>
2019-05-14 16:52:48 +0800

    1

--- src/main/java/com/giveup/JdbcUtils.java
+++ src/main/java/com/giveup/JdbcUtils.java
@@ -415,11 +415,11 @@ public class JdbcUtils {
 
 		if (ins == null || ins.length == 0)
 			return "";
-		columnEq = columnEq == null ? "1" : columnEq;
+		columnEq = (columnEq == null || andOr.trim().isEmpty()) ? "1" : columnEq;
 		if (!columnEq.equals("1") && !columnEq.equals("0"))
 			return "";
 
-		andOr = andOr == null || andOr.trim().isEmpty() ? "and" : andOr;
+		andOr = (andOr == null || andOr.trim().isEmpty()) ? "and" : andOr;
 		if (!andOr.equalsIgnoreCase("and") && !andOr.equalsIgnoreCase("or"))
 			return "";
 		sqlPart = andOr + " ";
