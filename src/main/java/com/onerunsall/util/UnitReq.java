94a1b080f73dc6ca44ee96a4ac0c7387fa29e1d6
renxinwei <413038044@qq.com>
2019-08-23 09:25:02 +0800

    1

--- src/main/java/com/onerunsall/util/UnitReq.java
+++ src/main/java/com/onerunsall/util/UnitReq.java
@@ -1,57 +1,44 @@
 package com.onerunsall.util;
 
-import java.sql.Connection;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.apache.commons.lang3.RandomStringUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.apache.log4j.Logger;
-
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import com.alibaba.fastjson.serializer.SerializerFeature;
-import com.onerunsall.util.Goback;
-import com.onerunsall.util.JdbcUtils;
-import com.onerunsall.util.ResBean;
-import com.onerunsall.util.ServletUtils;
 
 public class UnitReq {
 	public static Logger logger = Logger.getLogger(UnitReq.class);
 	private static SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmssSSS");
-	protected Date reqTime = new Date();
-	protected Date resTime = null;
-	protected String reqId = sdf.format(reqTime) + RandomStringUtils.randomNumeric(6);
+	public Date reqTime = new Date();
+	public Date resTime = null;
+	public String reqId = sdf.format(reqTime) + RandomStringUtils.randomNumeric(6);
 
-	protected String reqSign = "";
+	protected String reqName = "";
 	protected String reqStr = "";
 	protected String resStr = "";
 
 	protected UnitReq() {
 	}
 
-	public UnitReq(String reqSign, Object... params) {
-		this.reqSign = reqSign;
+	public UnitReq(String reqName, Object... params) {
+		this.reqName = reqName;
 		for (int i = 0; i < params.length; i++) {
 			this.reqStr = this.reqStr + (i + 1) + "-" + params[i].toString() + " ";
 		}
-		logger.info(this.reqSign + " " + this.reqId + " " + this.reqStr);
+		logger.info(this.reqId + " " + this.reqName);
+		logger.info(this.reqId + " req : " + this.reqStr);
 	}
 
 	public void res() {
 		this.resTime = new Date();
-		logger.info(this.reqSign + " " + this.reqId + " " + this.resStr + " takes:"
-				+ (this.resTime.getTime() - this.reqTime.getTime()) + "ms");
+		logger.info(this.reqId + " res : " + this.resStr + " takes:" + (this.resTime.getTime() - this.reqTime.getTime())
+				+ "ms");
 	}
 
 	public void res(Object param) {
 		this.resTime = new Date();
 		this.resStr = param.toString();
-		logger.info(this.reqSign + " " + this.reqId + " " + this.resStr + " takes:"
+		logger.info(this.reqId + " res : " + this.resStr + " takes : "
 				+ (this.resTime.getTime() - this.reqTime.getTime()) + "ms");
 	}
 }
