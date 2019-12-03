33d8e175bcb2302a93bb0b1ac1b6fdbe1c4b60c4
renxinwei <413038044@qq.com>
2019-12-03 22:33:06 +0800

    1

--- src/main/java/com/oneall/util/JavaMailUtil.java
+++ src/main/java/com/oneall/util/JavaMailUtil.java
@@ -10,80 +10,34 @@ import javax.mail.internet.MimeMessage;
 
 public class JavaMailUtil {
 	public static void main(String[] args) throws Exception {
-		JavaMailUtil.Heart hert = new JavaMailUtil.Heart();
-		hert.setHost("smtp.qq.com");
-		hert.setProtocol("smtp");
-		hert.setPwd("jthdzmjaheysbiga");
-		hert.setSender("413038044@qq.com");
-		hert.setSmtpAuth("true");
-		hert.setUser("413038044@qq.com");
+		JavaMailUtil j = new JavaMailUtil("smtp.qq.com", "smtp", "true", "413038044@qq.com", "pzivczucpondbhhh",
+				"413038044@qq.com");
 
-		JavaMailUtil.send(hert, "413038044@qq.com", "ad", "asdf");
+		j.send("413038044@qq.com", "aq123123d", "qqqq");
 	}
 
-	public static class Heart {
-		String host;
-		String protocol;
-		String smtpAuth;
-		String user;
-		String pwd;
-		String sender;
-
-		public String getHost() {
-			return host;
-		}
-
-		public void setHost(String host) {
-			this.host = host;
-		}
-
-		public String getProtocol() {
-			return protocol;
-		}
-
-		public void setProtocol(String protocol) {
-			this.protocol = protocol;
-		}
-
-		public String getSmtpAuth() {
-			return smtpAuth;
-		}
-
-		public void setSmtpAuth(String smtpAuth) {
-			this.smtpAuth = smtpAuth;
-		}
-
-		public String getUser() {
-			return user;
-		}
-
-		public void setUser(String user) {
-			this.user = user;
-		}
-
-		public String getPwd() {
-			return pwd;
-		}
-
-		public void setPwd(String pwd) {
-			this.pwd = pwd;
-		}
-
-		public String getSender() {
-			return sender;
-		}
-
-		public void setSender(String sender) {
-			this.sender = sender;
-		}
-
+	public JavaMailUtil(String host, String protocol, String smtpAuth, String user, String pwd, String sender) {
+		super();
+		this.host = host;
+		this.protocol = protocol;
+		this.smtpAuth = smtpAuth;
+		this.user = user;
+		this.pwd = pwd;
+		this.sender = sender;
 	}
 
-	public static Message send(Heart heart, String receiver, String subject, String content) throws Exception {
+	private String host;
+	private String protocol;
+	private String smtpAuth;
+	private String user;
+	private String pwd;
+	private String sender;
+
+	public Message send(String receiver, String subject, String content) throws Exception {
 		Properties prop = new Properties();
-		prop.setProperty("mail.host", heart.host);
-		prop.setProperty("mail.transport.protocol", heart.protocol);
-		prop.setProperty("mail.smtp.auth", heart.smtpAuth);
+		prop.setProperty("mail.host", host);
+		prop.setProperty("mail.transport.protocol", protocol);
+		prop.setProperty("mail.smtp.auth", smtpAuth);
 		// 使用JavaMail发送邮件的5个步骤
 		// 1、创建session
 		Session session = Session.getInstance(prop);
@@ -92,13 +46,13 @@ public class JavaMailUtil {
 		// 2、通过session得到transport对象
 		Transport ts = session.getTransport();
 		// 3、连上邮件服务器
-		ts.connect(heart.host, heart.user, heart.pwd);
+		ts.connect(host, user, pwd);
 
 		// 4、创建邮件
 		// 创建邮件对象
 		MimeMessage message = new MimeMessage(session);
 		// 指明邮件的发件人
-		message.setFrom(new InternetAddress(heart.sender));
+		message.setFrom(new InternetAddress(sender));
 		// 指明邮件的收件人，现在发件人和收件人是一样的，那就是自己给自己发
 		message.setRecipient(Message.RecipientType.TO, new InternetAddress(receiver));
 		// 邮件的标题
