caff1cd476bee4e32ef5ebd279ae8e462d231342
renxinwei <413038044@qq.com>
2019-12-04 11:48:48 +0800

    1

--- src/main/java/com/oneall/util/MapChain.java
+++ src/main/java/com/oneall/util/MapChain.java
@@ -1,5 +1,6 @@
 package com.oneall.util;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class MapChain<K, V> {
@@ -9,6 +10,11 @@ public class MapChain<K, V> {
 	public MapChain(Map<K, V> map) {
 		super();
 		this.map = map;
+	}
+
+	public MapChain() {
+		super();
+		this.map = new HashMap<K, V>();
 	}
 
 	public MapChain<K, V> put(K key, V value) {
