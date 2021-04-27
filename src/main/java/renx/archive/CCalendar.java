2ab511cf1d8a90c6a87c900574e179d0aa1a4260
renx <413038044@qq.com>
2021-04-27 14:05:14 +0800

    2.2.5

--- src/main/java/renx/archive/CCalendar.java
+++ src/main/java/renx/archive/CCalendar.java
@@ -26,6 +26,17 @@ public class CCalendar {
 		return this;
 	}
 
+	public CCalendar set(int field, int value) {
+		this.real.set(field, value);
+		return this;
+	}
+
+	public CCalendar settime(int hour, int minute, int second, int millisecond) {
+		set(Calendar.HOUR_OF_DAY, hour).set(Calendar.MINUTE, minute).set(Calendar.SECOND, second)
+				.set(Calendar.MILLISECOND, millisecond);
+		return this;
+	}
+
 	public Date time() {
 		return this.real.getTime();
 	}
