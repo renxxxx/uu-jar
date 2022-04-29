48324e934ed6ca3f1c9e86aa69d87334107dd559
renx <413038044@qq.com>
2022-04-29 10:58:23 +0800

    2.2.18

--- release.sh
+++ release.sh
@@ -3,12 +3,19 @@ group=renx
 artifact=uu
 
 version=`cat  ./src/main/java/renx/uu/UU.java |grep -oP '(?<= version = ").*(?=";)'`
-echo current version: $version; echo;
+echo current version: $version
+
+read -p "Enter new version: " newVersion
+
+if [[ "$newVersion" = "" ]]
+then 
+	newVersion=$version
+fi
 
 sed -i "0,/^\(.*\)<groupId>.*<\/groupId>\(.*\)$/s//\1<groupId>$group<\/groupId>/" ./pom.xml
-sed -i "0,/^\(.*\)<artifactId>.*<\/artifactId>\(.*\)$/s//\1<version>$artifactId<\/artifactId>/" ./pom.xml
-sed -i "0,/^\(.*\)<version>.*<\/version>\(.*\)$/s//\1<version>$version<\/version>/" ./pom.xml
-#sed -i "0,/^\(\s*\)public static String version = \".*\";$/s//\1public static String version = \"$version\";/" ./src/main/java/renx/uu/UU.java
+sed -i "0,/^\(.*\)<artifactId>.*<\/artifactId>\(.*\)$/s//\1<artifactId>$artifactId<\/artifactId>/" ./pom.xml
+sed -i "0,/^\(.*\)<version>.*<\/version>\(.*\)$/s//\1<version>$newVersion<\/version>/" ./pom.xml
+#sed -i "0,/^\(\s*\)public static String version = \".*\";$/s//\1public static String version = \"$newVersion\";/" ./src/main/java/renx/uu/UU.java
 
 echo "-mvn -q clean install"
 mvn -q clean install
@@ -35,7 +42,7 @@ git push
 echo
 
 echo "-rename package"
-packageName=$group-$artifact-$version.jar
+packageName=$group-$artifact-$newVersion.jar
 mv target/*.jar target/$packageName
 echo
 
