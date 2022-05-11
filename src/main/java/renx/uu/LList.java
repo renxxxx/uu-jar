0455bfa82d44482d56c51cf27643ef6908362aec
renx <413038044@qq.com>
2022-05-11 12:37:27 +0800

    2.3.3

--- src/main/java/renx/uu/LList.java
+++ src/main/java/renx/uu/LList.java
@@ -33,9 +33,26 @@ public class LList<E> {
 		return build(null);
 	}
 
+	public LList add(int i, Object e, boolean if_) {
+		if (if_)
+			return add(i, e);
+		return this;
+	}
+
 	public LList add(Object e, boolean if_) {
 		if (if_)
 			return add(e);
+		return this;
+	}
+
+	public LList add(int i, Object e) {
+		if (this.list == null)
+			this.list = new ArrayList();
+		if (e instanceof MMap)
+			e = ((MMap) e).map;
+		else if (e instanceof LList)
+			e = ((LList) e).list;
+		this.list.add(i, e);
 		return this;
 	}
 
@@ -110,9 +127,22 @@ public class LList<E> {
 			return this.list.get(index);
 	};
 
+	public String getString(int index) {
+		if (this.list == null || this.list.size() <= index)
+			return null;
+		else
+			return Var.toString(this.list.get(index));
+	};
+
 	public Object next() {
 		if (this.list != null && (this.list.size() - 1) >= (this.index + 1))
 			return get(++this.index);
+		return null;
+	};
+
+	public String nextString() {
+		if (this.list != null && (this.list.size() - 1) >= (this.index + 1))
+			return getString(++this.index);
 		return null;
 	};
 
