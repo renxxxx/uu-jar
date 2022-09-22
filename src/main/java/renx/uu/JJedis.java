8455eb556ed061fde031797a5cfdaa5dac04f17e
renx <413038044@qq.com>
2022-09-22 12:10:24 +0800

    2.7.0.72

--- src/main/java/renx/uu/JJedis.java
+++ src/main/java/renx/uu/JJedis.java
@@ -1,5 +1,8 @@
 package renx.uu;
 
+import java.sql.SQLException;
+import java.util.Collections;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -109,70 +112,38 @@ public class JJedis {
 		}
 	}
 
-	public boolean lock(String lockName) {
-		logger.info("start lock " + runId);
+	public boolean lock(String lock, String owner, long acquireTime, long period)
+			throws SQLException, InterruptedException {
 		open();
-		long acquireTimeout = 0;
-		long lockTimeout = 0;
-
-		String realLockName = prefix + lockName;
-		logger.info("lock realLockName " + realLockName);
-		String theadId = String.valueOf(Thread.currentThread().getId());
-		if (acquireTimeout == 0) {
-			// 如未设置锁获取超时时间默认为10秒
-			acquireTimeout = 10000;
-		}
-		if (lockTimeout == 0) {
-			// 如未设置锁超时时间默认未60秒
-			lockTimeout = 60000;
-		}
-		int lockTimeoutMin = (int) (lockTimeout / 1000);
-		long acquireEndTime = System.currentTimeMillis() + acquireTimeout;
-		// 防止未设置时间产生死锁
-		if (jedis.ttl(realLockName) == -1) {
-			jedis.expire(realLockName, lockTimeoutMin);
-		}
-		while (System.currentTimeMillis() < acquireEndTime) {
-			// 加锁成功
-			if (theadId.equals(jedis.get(realLockName)) || jedis.setnx(realLockName, theadId) == 1) {
-				logger.info("lock realLockName " + realLockName + " true");
-				jedis.expire(realLockName, lockTimeoutMin);
+		int i = 0;
+		long seconds = acquireTime / 1000;
+		while (true) {
+			String result = jedis.set(lock, owner, "NX", "PX", period);
+			if ("OK".equals(result)) {
 				return true;
 			}
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+			i++;
+			if (i > seconds)
+				break;
+			Thread.sleep(1000);
 		}
-		logger.info("lock realLockName " + realLockName + " false");
 		return false;
 	}
 
-	public boolean unlock(String lockName) {
-		logger.info("start unlock " + runId);
+	public boolean lock(String lock, String owner) throws SQLException, InterruptedException {
+		return lock(lock, owner, 10 * 1000, 1 * 60 * 1000);
+	}
+
+	public boolean unlock(String lock, String owner) {
 		open();
-		String realLockName = prefix + lockName;
-		logger.info("unlock realLockName " + realLockName);
-		String theadId = String.valueOf(Thread.currentThread().getId());
-		boolean isRelease = false;
-		while (true) {
-			// jedis事务操作 监控realLockName
-			// 如果其中值发生变化tx.exec事务不会提交(注jedis中的事务不具备原子性,其中一个事务有错误，另外的仍然执行)
-			jedis.watch(realLockName);
-			if (theadId.equals(jedis.get(realLockName))) {
-				Transaction tx = jedis.multi();
-				tx.del(realLockName);
-				if (tx.exec().isEmpty()) {
-					continue;
-				}
-				isRelease = true;
-			}
-			// q取消监控
-			jedis.unwatch();
-			break;
+		String script = "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end";
+		Object result = jedis.eval(script, Collections.singletonList(lock), Collections.singletonList(owner));
+
+		Long RELEASE_SUCCESS = 1L;
+		if (RELEASE_SUCCESS.equals(result)) {
+			return true;
 		}
-		logger.info("unlock realLockName " + realLockName + " " + isRelease);
-		return isRelease;
+		return false;
+
 	}
 }
