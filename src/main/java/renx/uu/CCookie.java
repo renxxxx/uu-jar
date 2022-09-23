ebc7a5fe14312aac308675f5b56eb45a951480e3
renx <413038044@qq.com>
2022-09-23 09:04:48 +0800

    2.7.0.81

--- src/main/java/renx/uu/CCookie.java
+++ src/main/java/renx/uu/CCookie.java
@@ -1,11 +1,16 @@
 package renx.uu;
 
+import java.util.Map;
+
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class CCookie {
 
 	public Cookie o = null;
+
+	public String name = null;
 
 	public static void main(String[] args) {
 		new Cookie("asdf", null);
@@ -19,17 +24,27 @@ public class CCookie {
 	public static CCookie build(Cookie o) {
 		CCookie ccookie = new CCookie();
 		ccookie.o = o;
+		if (o != null)
+			ccookie.name = o.getName();
 		return ccookie;
 	}
 
 	public static CCookie build(String name, String value) {
 		CCookie ccookie = new CCookie();
 		ccookie.o = new Cookie(name, value);
+		ccookie.name = name;
+		return ccookie;
+	}
+
+	public static CCookie build(String name) {
+		CCookie ccookie = new CCookie();
+		ccookie.name = name;
 		return ccookie;
 	}
 
 	public CCookie domain(String domain) {
-		o.setDomain(domain);
+		if (o != null)
+			o.setDomain(domain);
 		return this;
 	}
 
@@ -38,7 +53,8 @@ public class CCookie {
 	}
 
 	public CCookie path(String uri) {
-		o.setPath(uri);
+		if (o != null)
+			o.setPath(uri);
 		return this;
 	}
 
@@ -47,12 +63,20 @@ public class CCookie {
 	}
 
 	public CCookie httpOnly(boolean isHttpOnly) {
-		o.setHttpOnly(isHttpOnly);
+		if (o != null)
+			o.setHttpOnly(isHttpOnly);
 		return this;
 	}
 
 	public CCookie maxAge(int expiry) {
-		o.setMaxAge(expiry);
+		if (o != null)
+			o.setMaxAge(expiry);
+		return this;
+	}
+
+	public CCookie value(HttpServletRequest request) {
+		Map<String, Cookie> cookiem = Servletuu.cookiem(request);
+		this.o = cookiem.get(name);
 		return this;
 	}
 
@@ -62,7 +86,29 @@ public class CCookie {
 	}
 
 	public CCookie value(String newValue) {
-		o.setValue(newValue);
+		if (o != null)
+			o.setValue(newValue);
+		return this;
+	}
+
+	public String name() {
+		return name;
+	}
+
+	public CCookie name(String newName) {
+		name = newName;
+		if (o != null) {
+			Cookie o2 = new Cookie(newName, o.getValue());
+			o2.setComment(o.getComment());
+			o2.setDomain(o.getDomain());
+			o2.setHttpOnly(o.isHttpOnly());
+			o2.setMaxAge(o.getMaxAge());
+			o2.setPath(o.getPath());
+			o2.setSecure(o.getSecure());
+			o2.setValue(o.getValue());
+			o2.setVersion(o.getVersion());
+			o = o2;
+		}
 		return this;
 	}
 
@@ -71,12 +117,14 @@ public class CCookie {
 	}
 
 	public CCookie secure(boolean flag) {
-		o.setSecure(flag);
+		if (o != null)
+			o.setSecure(flag);
 		return this;
 	}
 
 	public CCookie addTo(HttpServletResponse response) {
-		response.addCookie(o);
+		if (o != null)
+			response.addCookie(o);
 		return this;
 	}
 
